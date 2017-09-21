b004a324e448b77f880b6958bfcf2b9cce502760
Manfred Kaul <Manfred.Kaul@gmail.com>
2017-09-21 18:33:26 +0200

    ccm wrapper for highlight.js

---
+++ highlight/ccm.highlight.js
@@ -0,0 +1,56 @@
+/**
+ * @overview ccm component for highlight
+ * @author Manfred Kaul <manfred.kaul@h-brs.de> 2017
+ * @license The MIT License (MIT)
+ */
+
+( function () {
+
+  var component  = {
+
+    name: 'highlight',
+    
+    ccm: 'https://akless.github.io/ccm/ccm.js',
+
+    config: {
+      css: [ 'ccm.load',  './resources/default.min.css' ],
+      hljs: [ 'ccm.load',  './resources/highlight.min.js' ]
+      // css: [ 'ccm.load',  'https://mkaul.github.io/ccm-components/highlight/resources/default.css' ],
+      // user:   [ 'ccm.instance', 'https://akless.github.io/ccm-components/user/versions/ccm.user-1.0.0.min.js' ],
+      // logger: [ 'ccm.instance', 'https://akless.github.io/ccm-components/log/versions/ccm.log-1.0.0.min.js', [ 'ccm.get', 'https://akless.github.io/ccm-components/log/resources/log_configs.min.js', 'greedy' ] ],
+      // onfinish: function( instance, results ){ console.log( results ); }
+    },
+
+    Instance: function () {
+    
+      var self = this;
+
+      this.start = function ( callback ) {
+      
+        // has logger instance? => log 'render' event
+        if ( self.logger ) self.logger.log( 'render' );
+        
+        // prepare main HTML structure
+        var main_html = '<pre><code class="'
+          + self.root.classList.value
+          + '">'
+          + self.inner.textContent
+          + '</code></pre>';
+        
+        // set content of own website area
+        self.ccm.helper.setContent( self.element, main_html );
+        
+        // get DOM element of <pre><code>
+        var main_elem = self.element.querySelector('pre code');
+  
+        hljs.highlightBlock( main_elem );
+
+        if ( callback ) callback();
+      };
+
+    }
+
+  };
+
+  function p(){window.ccm[v].component(component)}var f="ccm."+component.name+(component.version?"-"+component.version.join("."):"")+".js";if(window.ccm&&null===window.ccm.files[f])window.ccm.files[f]=component;else{var n=window.ccm&&window.ccm.components[component.name];n&&n.ccm&&(component.ccm=n.ccm),"string"==typeof component.ccm&&(component.ccm={url:component.ccm});var v=component.ccm.url.split("/").pop().split("-");if(v.length>1?(v=v[1].split("."),v.pop(),"min"===v[v.length-1]&&v.pop(),v=v.join(".")):v="latest",window.ccm&&window.ccm[v])p();else{var e=document.createElement("script");document.head.appendChild(e),component.ccm.integrity&&e.setAttribute("integrity",component.ccm.integrity),component.ccm.crossorigin&&e.setAttribute("crossorigin",component.ccm.crossorigin),e.onload=function(){p(),document.head.removeChild(e)},e.src=component.ccm.url}}
+}() );
