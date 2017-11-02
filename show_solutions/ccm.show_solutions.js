6c2195e5889dc3510e64824f14f4fbc5969b5719
Manfred Kaul <Manfred.Kaul@gmail.com>
2017-11-02 14:25:36 +0100

    show_solutions enhanced with ccm editor (Quill)

--- show_solutions/ccm.show_solutions.js
+++ show_solutions/ccm.show_solutions.js
@@ -30,7 +30,8 @@
           inner: [
             { tag: 'input', id: '%id%', type: 'checkbox' },
             { tag: 'label', for: '%id%', inner: 'Zeige Liste aller eingereichten Lösungen' },
-            { class: 'solutions' }
+            { class: 'solutions' },
+            { tag: 'a', class: 'more', inner: { tag: 'i', inner: '...more' } }
           ]
         },
         error: {
@@ -83,6 +84,36 @@
         user: [ 'ccm.instance', 'https://akless.github.io/ccm-components/user/versions/ccm.user-2.0.0.min.js' ],
         logger: [ 'ccm.instance', 'https://akless.github.io/ccm-components/log/versions/ccm.log-1.0.0.min.js', [ 'ccm.get', 'https://kaul.inf.h-brs.de/data/2017/se1/json/log_configs.js', 'se_ws17_voting' ] ]
       } ],
+
+      editor: [ 'ccm.component', 'https://tkless.github.io/ccm-components/editor/versions/ccm.editor-1.0.0.js', {
+        settings: {
+          modules: {
+            history: {
+              delay: 1000,
+              maxStack: 300,
+              userOnly: false // default
+            },
+            //syntax: true,    // needed for syntax highlighting
+
+            toolbar: [          // if no toolbar needed set: settings.modules.toolbar: false
+              // [ { 'header': [1, 2, 3, 4, 5, 6, false] } ],
+              [ 'bold', 'italic', 'underline', 'strike' ],        // toggled buttons
+              [ { 'size': ['small', false, 'large', 'huge'] } ],
+              // [ { 'list': 'ordered'}, { 'list': 'bullet' } ],
+              [ { 'indent': '-1'}, { 'indent': '+1' } ],
+              [ { 'color': [] }, { 'background': [] } ],
+              // [ { 'font': [] } ],
+              // [ { 'align': [] } ],
+              [ 'link' ],   // , 'image' => beware of large data volume
+              // [ 'clean' ]
+            ]
+          },
+          placeholder: 'Inhaltsbereich für den Editor',
+          theme: 'snow'
+        }
+      } ],
+
+      uml:    [ 'ccm.component', 'https://kaul.inf.h-brs.de/data/ccm/uml/ccm.uml.js' ],
       
       logger: [ 'ccm.instance', 'https://akless.github.io/ccm-components/log/versions/ccm.log-1.0.0.min.js', [ 'ccm.get', 'https://kaul.inf.h-brs.de/data/2017/se1/json/log_configs.js', 'se_ws17_show_solutions' ] ]
       
@@ -111,6 +142,14 @@
         
         // select solutions div
         var solutions_div = main_elem.querySelector( '.solutions' );
+        var more_link = main_elem.querySelector( '.more' );
+        more_link.style.display = 'none';
+        var offset = 1;
+
+        more_link.addEventListener('click', e => {
+          offset += 10;
+          // proceed();
+        });
         
         // select checkbox
         var checkbox = main_elem.querySelector( 'input' );
@@ -121,13 +160,9 @@
             // has logger instance? => log 'show' event
             if ( self.logger ) self.logger.log( 'show', { component: self.index, fkey: self.fkey, for: self.for } );
             
-            if ( solutions_div.style.display === 'none' ) {
-              
-              solutions_div.style.display = 'block';
-              
-            }
-            else {
-
+            if ( solutions_div.style.display === 'none' && solutions_div.firstElementChild.classList.value !== "ccm_loading" ) {
+                    solutions_div.style.display = 'block';
+            } else {
               // create a loading symbol
               let loading = self.ccm.helper.loading( self );
               self.ccm.helper.setContent( solutions_div, loading );
@@ -146,7 +181,7 @@
                     token: self.user.data().token,
                     semester: self.keys.semester,
                     fach: self.keys.fach,
-                    all: 1
+                    all: offset
                   }
                 };
                 
@@ -177,7 +212,9 @@
                       if ( uid !== 'ERROR' &&  uid !== 'SUCCESS' ){
                         
                         var child;
+                        var newChild;
                         var solution = record[ uid ];
+                        var solution_object;
                         
                         if ( json ){
                           // Is field a JSON structure?
@@ -192,7 +229,42 @@
                           
                         } else { // no JSON
                           child = self.ccm.helper.html( self.html.solution );
-                          child.querySelector('textarea').value = solution;
+                          switch (typing(solution)){
+                            case "string":
+                            child.querySelector('textarea').value = solution;
+                              break;
+                            case "json":
+                              solution_object = JSON.parse( solution );
+                              if ( solution_object.ops ){
+                                newChild = document.createElement('div');
+                                child.replaceChild( newChild, child.firstElementChild );
+                                self.editor.start( { root: newChild }, instance => {
+                                  instance.get().setContents( solution_object );
+                                });
+                              } else if ( solution_object.uml ){
+                                newChild = document.createElement('div');
+                                child.replaceChild( newChild, child.firstElementChild );
+                                self.uml.start({root: newChild, default: solution_object.uml }, instance => {
+                                  instance.sync( solution_object.uml );
+                                });
+                              } else if ( solution_object.type && solution_object.type.startsWith('image') ){ // upload
+                                newChild = document.createElement('img');
+                                newChild.style.width = '96%';
+                                newChild.style.height = '600px';
+                                newChild.src = get_url({ url: self.server, params: { semester: self.keys.semester, fach: self.keys.fach, key: self.fkey, id: self.for, uid: uid, user: self.user.data().id, token: self.user.data().token } });
+                                child.replaceChild( newChild, child.firstElementChild );
+                              }
+                              break;
+                            case "editor":
+                              break;
+                            case "uml":
+                              break;
+                            case "upload":
+                              break;
+                            default:
+                              debugger;
+                          }
+
                         }
                         
                         counter++;
@@ -222,6 +294,8 @@
                       sorted.map( function ( entry ) {
                         solutions_div.appendChild( entry.solution );
                       } );
+
+                      more_link.style.display = 'block';
                       
                       function compare( a, b ) {
                         if ( a.voting > b.voting )
@@ -250,6 +324,36 @@
         
         // set content of own website area
         self.ccm.helper.setContent( self.element, main_elem );
+
+        // =============== helper functions ================
+        function typing( solution ){
+          var typ = typeof solution;
+          if ( typ === "string" ){
+            if ( solution.startsWith('{') ) return "json";
+            else return "string"
+          }
+          if ( typ === "object" ){
+            if ( solution.ccm_type ) return solution.ccm_type; // ccm typing
+            if ( solution.uml ) return "uml";
+            if ( solution.ops ) return "editor";
+            if ( solution.type ) return "upload";
+            if ( Array.isArray( solution ) ) return "array";
+            else return "object";
+          }
+          return typ;
+        }
+
+        function get_url( args ) {
+          var url = args.url;
+          Object.keys( args.params ).map( (key, index) => {
+            url += index === 0 ? '?' : '&';
+            url += key;
+            url += '=';
+            url += args.params[key];
+          });
+          console.log( url );
+          return url;
+        }
         
         if ( callback ) callback();
       };
