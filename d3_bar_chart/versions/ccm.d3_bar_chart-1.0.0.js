4e9f54e0dffb8a1ae8d955bb78ef168054c76cb3
Manfred Kaul <Manfred.Kaul@gmail.com>
2017-11-17 13:10:45 +0100

    d3.js bar chart V 1.0.0

---
+++ d3_bar_chart/versions/ccm.d3_bar_chart-1.0.0.js
@@ -0,0 +1,131 @@
+/**
+ * @overview ccm component for d3_bar_chart
+ * @author Manfred Kaul <manfred.kaul@h-brs.de> 2017
+ * @license The MIT License (MIT)
+ * @version latest (1.0.0)
+ * TODO: docu comments -> API
+ * TODO: unit tests
+ * TODO: builder component
+ * TODO: i18n
+ */
+
+{
+
+  const component  = {
+
+    /**
+     * unique component name
+     * @type {string}
+     */
+    name: 'd3_bar_chart',
+    
+    /**
+     * recommended used framework version
+     * @type {string}
+     */
+    ccm: 'https://akless.github.io/ccm/version/ccm-11.5.0.min.js',
+    // ccm: '//akless.github.io/ccm/ccm.js',
+
+    /**
+     * default instance configuration
+     * @type {object}
+     */
+    config: {
+
+      d3: [ "ccm.load", "//kaul.inf.h-brs.de/data/ccm/lib/d3.v4.min.js" ],
+
+      data: [4, 8, 15, 16, 23, 42],
+
+      html: {
+        main: {
+          id: "chart"
+        }
+
+      },
+
+      css: [ 'ccm.load',  '//kaul.inf.h-brs.de/data/ccm/d3_bar_chart/resources/default.css' ],
+      // css: [ 'ccm.load',  'https://mkaul.github.io/ccm-components/d3_bar_chart/resources/default.css' ],
+      // user:   [ 'ccm.instance', 'https://akless.github.io/ccm-components/user/versions/ccm.user-2.0.0.min.js' ],
+      // logger: [ 'ccm.instance', 'https://akless.github.io/ccm-components/log/versions/ccm.log-1.0.0.min.js', [ 'ccm.get', 'https://akless.github.io/ccm-components/log/resources/log_configs.min.js', 'greedy' ] ],
+      // onfinish: function( instance, results ){ console.log( results ); }
+    },
+
+    /**
+     * for creating instances of this component
+     * @constructor
+     */
+    Instance: function () {
+    
+      /**
+       * own reference for inner functions
+       * @type {Instance}
+       */
+      const self = this;
+      
+      /**
+       * shortcut to help functions
+       * @type {Object}
+       */
+      let $;
+      
+      /**
+       * init is called once after all dependencies are solved and is then deleted
+       * @param {function} callback - called after all synchronous and asynchronous operations are complete
+       */
+      this.init = callback => {
+      
+        //  Is content given via LightDOM (inner HTML of Custom Element)?
+        //  Then use it with higher priority
+        if ( self.inner && self.inner.innerHTML.trim() ) self.text = self.inner.innerHTML;
+        
+        // ToDo interprete LightDOM
+
+        callback();
+      };
+      
+      /**
+       * is called once after the initialization and is then deleted
+       * @param {function} callback - called after all synchronous and asynchronous operations are complete
+       */
+      this.ready = callback => {
+
+        // set shortcut to help functions
+        $ = self.ccm.helper;
+        
+        callback();
+      };  
+        
+      /**
+       * starts the instance
+       * @param {function} [callback] - called after all synchronous and asynchronous operations are complete
+       */
+      this.start = callback => {
+      
+        // has logger instance? => log 'start' event
+        if ( self.logger ) self.logger.log( 'start' );
+
+        // prepare main HTML structure
+        const main_elem = $.html( self.html.main );
+
+        d3.select( main_elem )
+          .selectAll( "div" )
+          .data( self.data )
+          .enter()
+          .append( "div" )
+          .style( "height", (d)=> d + "px" );
+        
+        // set content of own website area
+        $.setContent( self.element, main_elem );
+
+        // Hack in order to get SVG rendered inside the shadow root
+        // self.element.innerHTML += '';
+
+        if ( callback ) callback();
+      };
+
+    }
+
+  };
+
+  function p(){window.ccm[v].component(component)}const f="ccm."+component.name+(component.version?"-"+component.version.join("."):"")+".js";if(window.ccm&&null===window.ccm.files[f])window.ccm.files[f]=component;else{const n=window.ccm&&window.ccm.components[component.name];n&&n.ccm&&(component.ccm=n.ccm),"string"===typeof component.ccm&&(component.ccm={url:component.ccm});var v=component.ccm.url.split("/").pop().split("-");if(v.length>1?(v=v[1].split("."),v.pop(),"min"===v[v.length-1]&&v.pop(),v=v.join(".")):v="latest",window.ccm&&window.ccm[v])p();else{const e=document.createElement("script");document.head.appendChild(e),component.ccm.integrity&&e.setAttribute("integrity",component.ccm.integrity),component.ccm.crossorigin&&e.setAttribute("crossorigin",component.ccm.crossorigin),e.onload=function(){p(),document.head.removeChild(e)},e.src=component.ccm.url}}
+}
