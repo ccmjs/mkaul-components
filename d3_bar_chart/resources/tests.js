3edf63ece9c2becb75de0bb22de298835c002985
Manfred Kaul <Manfred.Kaul@gmail.com>
2017-11-17 08:45:30 +0100

    bar chart rendered with famous d3.js inside shadow dom

---
+++ d3_bar_chart/resources/tests.js
@@ -0,0 +1,38 @@
+
+/**
+ * @overview unit tests of ccm component for d3_bar_chart
+ * @author Manfred Kaul <manfred.kaul@h-brs.de> 2017
+ * @license The MIT License (MIT)
+ */
+
+ccm.files[ 'tests.js' ] = {
+  setup: ( suite, callback ) => {
+    suite.ccm.component( '../d3_bar_chart/ccm.d3_bar_chart.js', component => {
+      suite.component = component;
+      callback();
+    } );
+  },
+  fundamental: {
+    tests: {
+      componentName: suite => {
+        suite.component.instance( instance => suite.assertSame( 'd3_bar_chart', instance.component.name ) );
+      }
+    }
+  },
+  tests: {
+    oneInput: suite => {
+      suite.component.start( {
+        inner: suite.ccm.helper.html( {
+          tag: 'input',
+          type: 'text',
+          name: 'foo',
+          value: 'bar'
+        } ),
+        onfinish: ( instance, result ) => suite.assertEquals( { foo: 'bar' }, result )
+      }, instance => {
+        console.log( instance.element );
+        instance.element.querySelector( '#d3_bar_chart' ).click();
+      } );
+    }
+  }
+};
