e9b06a2a88f47b18fc35524ad46da4f9b9078ea3
Manfred Kaul <Manfred.Kaul@gmail.com>
2018-05-04 07:49:08 +0200

    demo of use of CSS custom properties

---
+++ custom_properties/ccm.custom_properties.js
@@ -0,0 +1,128 @@
+/**
+ * @overview ccm component for custom_properties
+ * @author Manfred Kaul <manfred.kaul@h-brs.de> 2018
+ * @license The MIT License (MIT)
+ * @version latest (1.0.0)
+ * TODO: docu comments -> API
+ * TODO: unit tests
+ * TODO: builder component
+ * TODO: i18n
+ */
+
+{
+
+  var component  = {   // const not working in Safari
+
+    /**
+     * unique component name
+     * @type {string}
+     */
+    name: 'custom_properties',
+    
+    /**
+     * recommended used framework version
+     * @type {string}
+     */
+    // ccm: 'https://ccmjs.github.io/ccm/versions/ccm-16.3.3.js',
+    ccm: 'https://ccmjs.github.io/ccm/ccm.js',
+
+    /**
+     * default instance configuration
+     * @type {object}
+     */
+    config: {
+      html: {
+        main: {
+          style: "--param: 200px; width: 90%; height: 200px; background-color: blue; border: thin solid black;",
+          inner: {
+            class: "sub",
+            style: "width: calc( 0.5 * var(--param)); height: calc( 0.5 * var(--param)); margin: calc( 0.1 * var(--param)); background-color: red; border: thin solid black;"
+          }
+        }
+      },
+      // css: [ 'ccm.load',  '//kaul.inf.h-brs.de/data/ccmjs/mkaul-components/custom_properties/resources/default.css' ],
+      // user:   [ 'ccm.instance', 'https://akless.github.io/ccm-components/user/versions/ccm.user-2.0.0.min.js' ],
+      // logger: [ 'ccm.instance', 'https://akless.github.io/ccm-components/log/versions/ccm.log-1.0.0.min.js', [ 'ccm.get', 'https://akless.github.io/ccm-components/log/resources/log_configs.min.js', 'greedy' ] ],
+      // onfinish: function( instance, results ){ console.log( results ); }
+    },
+
+    /**
+     * for creating instances of this component
+     * @constructor
+     */
+    Instance: function () {
+    
+      /**
+       * own reference for inner functions
+       * @type {Instance}
+       */
+      const self = this;
+      
+      /**
+       * shortcut to help functions
+       * @type {Object}
+       */
+      let $;
+      
+      /**
+       * init is called once after all dependencies are solved and is then deleted
+       * @param {function} callback - called after all synchronous and asynchronous operations are complete
+       */
+      this.init = callback => {
+      
+        //  Is content given via LightDOM (inner HTML of Custom Element)?
+        //  Then use it with higher priority
+        if ( self.inner && self.inner.innerHTML.trim() ) self.text = self.inner.innerHTML;
+        
+        // ToDo interprete LightDOM
+
+        callback();
+      };
+      
+      /**
+       * is called once after the initialization and is then deleted
+       * @param {function} callback - called after all synchronous and asynchronous operations are complete
+       */
+      this.ready = callback => {
+
+        // set shortcut to help functions
+        $ = self.ccm.helper;
+        
+        callback();
+      };  
+        
+      /**
+       * starts the instance
+       * @param {function} [callback] - called after all synchronous and asynchronous operations are complete
+       */
+      this.start = callback => {
+      
+        // has logger instance? => log 'start' event
+        if ( self.logger ) self.logger.log( 'start' );
+        
+        // prepare main HTML structure
+        const main_elem = $.html( self.html.main );
+        const sub_elem = main_elem.querySelector(".sub");
+
+        window.addEventListener("resize", e => {
+          "use strict";
+          main_elem.style.setProperty('--param', main_elem.clientWidth + 'px');
+          if ( main_elem.clientWidth < 500 ){
+            sub_elem.style.setProperty( 'background-color', 'green' );
+          } else {
+            sub_elem.style.setProperty( 'background-color', 'red' );
+          }
+        });
+
+        // set content of own website area
+        $.setContent( self.element, main_elem );
+
+        if ( callback ) callback();
+      };
+
+    }
+
+  };
+
+  function p(){window.ccm[v].component(component)}const f="ccm."+component.name+(component.version?"-"+component.version.join("."):"")+".js";if(window.ccm&&null===window.ccm.files[f])window.ccm.files[f]=component;else{const n=window.ccm&&window.ccm.components[component.name];n&&n.ccm&&(component.ccm=n.ccm),"string"===typeof component.ccm&&(component.ccm={url:component.ccm});var v=component.ccm.url.split("/").pop().split("-");if(v.length>1?(v=v[1].split("."),v.pop(),"min"===v[v.length-1]&&v.pop(),v=v.join(".")):v="latest",window.ccm&&window.ccm[v])p();else{const e=document.createElement("script");document.head.appendChild(e),component.ccm.integrity&&e.setAttribute("integrity",component.ccm.integrity),component.ccm.crossorigin&&e.setAttribute("crossorigin",component.ccm.crossorigin),e.onload=function(){p(),document.head.removeChild(e)},e.src=component.ccm.url}}
+}
