79fd46320fc73c09a591582feafbf647c0886dd2
Manfred Kaul <Manfred.Kaul@gmail.com>
2018-10-10 08:55:05 +0200

    add key to configs

--- cryptoblock/resources/configs.js
+++ cryptoblock/resources/configs.js
@@ -6,26 +6,32 @@
 
 ccm.files[ 'configs.js' ] = {
   "difficulty1": {
+    "key": "difficulty1",
     "difficulty": 1, // number of zeros required at front of hash
     "maximumNonce": 600
   },
   "difficulty2": {
+    "key": "difficulty2",
     "difficulty": 2, // number of zeros required at front of hash
     "maximumNonce": 6000
   },
   "difficulty3": {
+    "key": "difficulty3",
     "difficulty": 3, // number of zeros required at front of hash
     "maximumNonce": 60000
   },
   "difficulty4": {
+    "key": "difficulty4",
     "difficulty": 4, // number of zeros required at front of hash
     "maximumNonce": 600000
   },
   "difficulty5": {
+    "key": "difficulty5",
     "difficulty": 5, // number of zeros required at front of hash
     "maximumNonce": 60000000
   },
   "difficulty6": {
+    "key": "difficulty6",
     "difficulty": 6, // number of zeros required at front of hash
     "maximumNonce": 60000000000
   }
