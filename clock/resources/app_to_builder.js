76f264f95cc02dd1ecf0f806b9332cd6fc14c429
Manfred Kaul <Manfred.Kaul@gmail.com>
2018-10-29 07:08:58 +0100

    add app_to_builder converter

---
+++ clock/resources/app_to_builder.js
@@ -0,0 +1,15 @@
+/**
+ * @overview helper function for app builder
+ * @version 0.0.1
+ * @author mkaul2m Manfred.Kaul@h-brs.de
+ * @copyright The MIT License (MIT) mkaul2m on 29.10.18.
+ */
+
+export function app_to_builder( json ) {
+  const old_width = json.width;
+  json.width = parseInt(old_width);
+  json.unit = old_width.slice(-2);
+  return json;
+}
+ 
+ 
--- clock/resources/clockBuilder.js
+++ clock/resources/clockBuilder.js
@@ -1,5 +1,5 @@
 /**
- * @overview description of the component
+ * @overview helper function for app builder
  * @version 0.0.1
  * @author mkaul2m Manfred.Kaul@h-brs.de
  * @copyright The MIT License (MIT) mkaul2m on 28.10.18.
