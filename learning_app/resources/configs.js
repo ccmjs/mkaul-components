3f0196dcab4961871c19a59d1721cedd6c736ac1
Manfred Kaul <Manfred.Kaul@gmail.com>
2018-10-29 21:06:03 +0100

    add app examples

--- learning_app/resources/configs.js
+++ learning_app/resources/configs.js
@@ -5,6 +5,42 @@
  */
 
 ccm.files[ 'configs.js' ] = {
+  "Paare_zuordnen": {
+    "key": "Paare_zuordnen",
+    "learn_id": "42536"
+  },
+  "Gruppenzuordnung": {
+    "key": "Gruppenzuordnung",
+    "learn_id": "1116045"
+  },
+  "Zahlenstrahl": {
+    "key": "Zahlenstrahl",
+    "learn_id": "2004599"
+  },
+  "Einfache_Reihenfolge": {
+    "key": "Einfache_Reihenfolge",
+    "learn_id": "2205378"
+  },
+  "Freie_Textantwort": {
+    "key": "Freie_Textantwort",
+    "learn_id": "5608699"
+  },
+  "Zuordnung_auf_Bild": {
+    "key": "Zuordnung_auf_Bild",
+    "learn_id": "2335599"
+  },
+  "Multiple_Choice_Quiz": {
+    "key": "Multiple_Choice_Quiz",
+    "learn_id": "157610"
+  },
+  "Lueckentext": {
+    "key": "Lueckentext",
+    "learn_id": "2541072"
+  },
+  "App_Matrix": {
+    "key": "App_Matrix",
+    "learn_id": "4539031"
+  },
   "demo": {
     "key": "demo",
     "learn_id": "1661469"
