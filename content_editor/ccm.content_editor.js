edda72a640ec360b0141b813d09bdee7a3d24c0c
Manfred Kaul <Manfred.Kaul@gmail.com>
2018-12-23 19:57:19 +0100

    support mobile devices with shift+click

--- content_editor/ccm.content_editor.js
+++ content_editor/ccm.content_editor.js
@@ -1047,7 +1047,7 @@
                   return all_attributes;
                 }, {}), component.config || {} );
               const instance = await component.start( config );
-              child.addEventListener('dblclick', openBuilder( instance, config ) );
+              child.addEventListener( isMobile() ? 'click' : 'dblclick', openBuilder( instance, config ) );
             } else {
               debugger;
             }
@@ -1339,7 +1339,7 @@
           editor_div.dispatchEvent(new Event('keyup'));
 
           root.firstChild.style = "display: inline-block;";
-          root.addEventListener('dblclick', openBuilder( instance, config ) );
+          root.addEventListener(isMobile() ? 'click' : 'dblclick', openBuilder( instance, config ) );
 
           // insert component at Cursor position or at the end of the text, if none
           const selection = editor_div.parentNode.parentNode.getSelection && editor_div.parentNode.parentNode.getSelection() || document.getSelection();
@@ -1372,53 +1372,58 @@
         function openBuilder( instance, config ){
           // persist the builder as property of instance
           return async event => {
-            if ( ! instance.json_builder )
-              instance.json_builder = await self.json_builder.start({
-                root: builder_div,
-                data: { // avoid solveDependency by storing in ccm.store
-                  store: [ 'ccm.store', { local: { app: JSON.parse( instance.config ) }}  ],
-                  key: 'app'
-                },
-                html: {
-                  "tag": "form",
-                  "onsubmit": "%onclick%",
-                  "inner": [
-                    {
-                      "tag": "textarea",
-                      "id": "input",
-                      "oninput": "%oninput%",
-                      "onchange": "%onchange%"
-                    },
-                    {
-                      "tag": "input",
-                      "id": "button",
-                      "type": "submit",
-                      "onclick": "%onclick%"
+            if ( event.type === 'dblclick' || ( isMobile() && event.type === 'click' && event.shiftKey ) ) {
+              if ( instance.json_builder ){
+                Object.assign( instance.json_builder, collectAttributes( instance ) );
+              } else {
+                instance.json_builder = await self.json_builder.start({
+                  root: builder_div,
+                  data: { // avoid solveDependency by storing in ccm.store
+                    store: [ 'ccm.store', { local: { app: JSON.parse( instance.config ) }}  ],
+                    key: 'app'
+                  },
+                  html: {
+                    "tag": "form",
+                    "onsubmit": "%onclick%",
+                    "inner": [
+                      {
+                        "tag": "textarea",
+                        "id": "input",
+                        "oninput": "%oninput%",
+                        "onchange": "%onchange%"
+                      },
+                      {
+                        "tag": "input",
+                        "id": "button",
+                        "type": "submit",
+                        "onclick": "%onclick%"
+                      }
+                    ]
+                  },
+                  onfinish: async (e) => {
+                    const json_builder_value = instance.json_builder.getValue();
+                    const instance_config = JSON.parse( instance.config );
+                    const all_diffs = compareJSON( instance_config, json_builder_value );
+
+                    for ( const [ name, diff ] of all_diffs ){
+                      instance.root.parentNode.setAttribute( name, diff );
                     }
-                  ]
-                },
-                onfinish: async (e) => {
-                  const json_builder_value = instance.json_builder.getValue();
-                  const instance_config = JSON.parse( instance.config );
-                  const all_diffs = compareJSON( instance_config, json_builder_value );
 
-                  for ( const [ name, diff ] of all_diffs ){
-                    instance.root.parentNode.setAttribute( name, diff );
+                    /********** Restart component instance **********/
+                    // await instance.start() is not necessary because of MutationObserver
+                    Object.assign( instance, await $.solveDependencies( json_builder_value ) );
+                    Object.assign( instance.json_builder, { data: {
+                        // avoid solveDependency by storing in ccm.store
+                        store: [ 'ccm.store', { local: { app: json_builder_value }}  ],
+                        key: 'app'
+                      } } );
+
+                    builder_div.style.display = 'none';
                   }
-
-                  /********** Restart component instance **********/
-                  // await instance.start() is not necessary because of MutationObserver
-                  Object.assign( instance, await $.solveDependencies( json_builder_value ) );
-                  Object.assign( instance.json_builder, { data: {
-                      // avoid solveDependency by storing in ccm.store
-                      store: [ 'ccm.store', { local: { app: json_builder_value }}  ],
-                      key: 'app'
-                    } } );
-
-                  builder_div.style.display = 'none';
-                }
-            });
-            builder_div.style.display = 'block';
+                })
+              }
+              builder_div.style.display = 'block';
+            }
           }
         }
 
@@ -1522,6 +1527,28 @@
         }
 
       };
+
+      /**
+       * collect all attributes from instance
+       * @param instance
+       */
+      function collectAttributes( instance ){
+        return [ ...instance.root.parentNode.attributes ].reduce((a,b)=>{
+          a[b.name] = b.value;
+          return a;
+        },{});
+      }
+
+      /**
+       * checks whether editor is run on a mobile platform
+       * @url https://stackoverflow.com/questions/11381673/detecting-a-mobile-browser
+       * @returns {boolean}
+       */
+      function isMobile() {
+        let check = false;
+        (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+        return check;
+      }
 
       /**
        * current state of this editor
