d2f67bfc4aab44b507d4b4a66513c1a42ebd739a
Manfred Kaul <Manfred.Kaul@gmail.com>
2019-04-30 18:31:55 +0200

    remove extra button and chart from simple demo

--- parkhaus/resources/configs.js
+++ parkhaus/resources/configs.js
@@ -53,6 +53,8 @@ ccm.files[ 'configs.js' ] = {
           { tag: 'div', class: '%extra_class%' }
         ] }
     },
+    extra_buttons: [],
+    extra_charts: [],
     traffic_light: {
       tag: 'svg', viewBox: '0 0 200 500', xmlns: 'http://www.w3.org/2000/svg', width: '40', height: '100', inner: [
         {
