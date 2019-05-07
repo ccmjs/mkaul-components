6889e28c38b38a07393c44754fa1ed05cbe4f037
Manfred Kaul <Manfred.Kaul@gmail.com>
2019-05-07 18:17:51 +0200

    double backslash in order to pass JSON.parse

--- regex/resources/configs.js
+++ regex/resources/configs.js
@@ -59,7 +59,7 @@ ccm.files[ 'configs.js' ] = {
   "zipcode": {
     key: "zipcode",
     data: {
-      regex: "^(D[- ]{0,1}){0,1}(?!00)\\d{2}\\d{3}$",
+      regex: "^(D[- ]{0,1}){0,1}(?!00)\\\\d{2}\\\\d{3}$",
       options: 'i',
       matching: "D-12345",
       non_matching: "00345"
@@ -69,7 +69,7 @@ ccm.files[ 'configs.js' ] = {
   "landline_number": {
     key: "landline_number",
     data: {
-      regex: "^0[\\d]{1,3}[-,\\s,/]?[\\d]{3,}$",
+      regex: "^0[\\\\d]{1,3}[-,\\\\s,/]?[\\\\d]{3,}$",
       options: 'i',
       matching: "0123-123456789",
       non_matching: "0-123"
@@ -79,7 +79,7 @@ ccm.files[ 'configs.js' ] = {
   "license_plate": {
     key: "license_plate",
     data: {
-      regex: "^[A-Z]{1,3}[-|\\s][A-Z]{1,3}[-|\\s][0-9]{1,4}$",
+      regex: "^[A-Z]{1,3}[-|\\\\s][A-Z]{1,3}[-|\\\\s][0-9]{1,4}$",
       options: 'i',
       matching: "K-AB 123",
       non_matching: "1234-567"
@@ -89,7 +89,7 @@ ccm.files[ 'configs.js' ] = {
   "email": {
     key: "email",
     data: {
-      regex: "^[\\w\\d-.]+@[-A-Za-z0-9]+\\.[A-Za-z]{2,5}$",
+      regex: "^[\\\\w\\\\d-.]+@[-A-Za-z0-9]+\\\\.[A-Za-z]{2,5}$",
       options: 'i',
       matching: "Karl.Mueller@h-brs.de",
       non_matching: "123@nix.germany"
@@ -99,7 +99,7 @@ ccm.files[ 'configs.js' ] = {
   "web_address": {
     key: "web_address",
     data: {
-      regex: "^(https?:\\/\\/)?([\\da-z\\.-]+)\\.([a-z\\.]{2,6})([\\/\\w \\.-]*)*$",
+      regex: "^(https?:\\\\/\\\\/)?([\\\\da-z\\\\.-]+)\\\\.([a-z\\\\.]{2,6})([\\\\/\\\\w \\\\.-]*)*$",
       options: 'i',
       matching: "https://kaul.inf.h-brs.de",
       non_matching: "file://abc.de"
