927d88e6e226b70551c5a4c8e9bffc0ac457cf75
Manfred Kaul <Manfred.Kaul@gmail.com>
2019-07-11 15:54:42 +0200

    change header

--- sophist/resources/configs.js
+++ sophist/resources/configs.js
@@ -8,58 +8,59 @@
 ccm.files[ 'configs.js' ] = {
 
   "demo": {
-    key: "demo",
-    data: {
-      "store": [ "ccm.store", 'https://ccmjs.github.io/mkaul-components/sophist/resources/datasets.js' ],
+    "key": "demo",
+    "html.main.inner.0.inner": "Demo",
+    "data": {
+      "store": [ "ccm.store", "https://ccmjs.github.io/mkaul-components/sophist/resources/datasets.js" ],
       "key": "demo"
     }
   },
 
   "ohne_Bedingung": {
-    key: "ohne_Bedingung",
-    data: {
-      "store": [ "ccm.store", 'https://ccmjs.github.io/mkaul-components/sophist/resources/datasets.js' ],
+    "key": "ohne_Bedingung",
+    "data": {
+      "store": [ "ccm.store", "https://ccmjs.github.io/mkaul-components/sophist/resources/datasets.js" ],
       "key": "demo"
     }
   },
 
   "mit_Bedingung": {
-    key: "mit_Bedingung",
-    headers: ["Bedingung","Verbindlichkeit","Systemname","Funktionalität","Objekt","Prozesswort", "Buttons"],
-    columns: ["condition","modal","system","func","object","process"],
-    initial_values: {
-      condition: "Bedingung",
-      system: "das System",
-      object: "Objekt",
-      process: "Prozesswort"
+    "key": "mit_Bedingung",
+    "headers": ["Bedingung","Verbindlichkeit","Systemname","Funktionalität","Objekt","Prozesswort", "Buttons"],
+    "columns": ["condition","modal","system","func","object","process"],
+    "initial_values": {
+      "condition": "Bedingung",
+      "system": "das System",
+      "object": "Objekt",
+      "process": "Prozesswort"
     },
-    data: {
-      "store": [ "ccm.store", 'https://ccmjs.github.io/mkaul-components/sophist/resources/datasets.js' ],
+    "data": {
+      "store": [ "ccm.store", "https://ccmjs.github.io/mkaul-components/sophist/resources/datasets.js" ],
       "key": "mit_Bedingung"
     }
   },
 
   "local_persistence": {
-    key: "local_persistence",
-    data: {
-      "store": [ "ccm.store", { name: 'sophist_db' } ],
-      key: "sophist_demo"
+    "key": "local_persistence",
+    "data": {
+      "store": [ "ccm.store", { "name": "sophist_db" } ],
+      "key": "sophist_demo"
     }
   },
 
   "remote_persistence": {
-    key: "remote_persistence",
-    data: {
-      "store": [ "ccm.store", { url: 'https://ccm2.inf.h-brs.de', name: 'sophist_db', dataset: 'sophist_demo' } ],
-      key: "sophist_demo"
+    "key": "remote_persistence",
+    "data": {
+      "store": [ "ccm.store", { "url": "https://ccm2.inf.h-brs.de", "name": "sophist_db", "dataset": "sophist_demo" } ],
+      "key": "sophist_demo"
     }
   },
 
   "websockets": {
-    key: "websockets",
-    data: {
-      "store": [ "ccm.store", { url: 'wss://ccm2.inf.h-brs.de', name: 'sophist_db', dataset: 'sophist_demo' } ],
-      key: "sophist_demo"
+    "key": "websockets",
+    "data": {
+      "store": [ "ccm.store", { "url": "wss://ccm2.inf.h-brs.de", "name": "sophist_db", "dataset": "sophist_demo" } ],
+      "key": "sophist_demo"
     }
   }
 
